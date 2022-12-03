112f6ab28c4e3d835cf9c4e0017b202023ec7826
Mikaeel <95021467+Mikaeelmoha123@users.noreply.github.com>
2022-12-03 08:18:20 -0800

    Add files via upload

---
+++ if statement.py
@@ -0,0 +1,23 @@
+from ctypes import memset
+from curses import meta
+from email.errors import CloseBoundaryNotFoundDefect
+from sys import meta_path
+
+
+i wake up
+if i'm hungry
+I eat breakfast
+
+i leave my house
+if its cloudy
+i bring an umbrella
+otherwise
+i bring sunglasses
+
+im at resturant
+if i wnat meat
+i order stake
+otherwise if i want pasta
+i order spaghetti % meatballs
+otherwise
+i order a salad
